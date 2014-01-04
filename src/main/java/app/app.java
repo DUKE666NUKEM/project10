607a7b204ca4afc1e80c7c7ee1b49def20b6cc94
Marat <marat.chardymov@gmail.com>
2014-01-05 02:08:37 +0300

    first commit

---
+++ src/main/java/app/app.java
@@ -0,0 +1,28 @@
+package app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class app {
+    public static void main(String[] args) {
+        try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            String database =
+                    "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};DBQ=FFS.mdb;";
+            Connection conn = DriverManager.getConnection(database, "", "");
+            Statement stmt = conn.createStatement();
+            stmt.execute("select * from Measurement");
+            ResultSet rs = stmt.getResultSet();
+            while(rs.next()){
+                System.out.println(rs.getString("Data"));
+            }
+            stmt.close();
+            conn.close();
+        } catch (Exception err) {
+            System.out.println("ERROR: " + err);
+
+        }
+    }
+}
